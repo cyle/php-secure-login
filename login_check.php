ef5df8525490bedbf84eb5d8b7383c91d993f172
Cyle <cylegage@gmail.com>
2013-07-09 19:17:00 -0400

    - updated session token system to be actually secure if someone gets the database, as the sessions are password-equivalent - updated user tables SQL to match -- will require rebuilding the database

--- login_check.php
+++ login_check.php
@@ -29,27 +29,41 @@ if (isset($_COOKIE['test_session']) && trim($_COOKIE['test_session']) != '') { /
 	
 	require_once('dbconn_mysql.php');
 	
-	$user_session_id = trim($_COOKIE['test_session']);
-	$user_session_id_db = "'".$mysqli->escape_string($user_session_id)."'";
-	
-	$check_for_session = $mysqli->query("SELECT * FROM user_sessions WHERE session_id=$user_session_id_db AND expires > UNIX_TIMESTAMP()");
-	if ($check_for_session->num_rows == 1) {
-		// oh snap -- they have a session!
-		$user_session_row = $check_for_session->fetch_assoc();
-		$current_user_id = $user_session_row['user_id'];
-		$current_user['loggedin'] = true;
-		$current_user['user_id'] = $current_user_id;
-		$new_session_key_expires = time() + (60*60*24*30);
-		setcookie('test_session', $user_session_id, $new_session_key_expires, '/', 'cylesoft.com');
-		$update_session_expiry = $mysqli->query("UPDATE user_sessions SET expires=$new_session_key_expires WHERE session_id=$user_session_id_db");
-		if ($_SERVER['PHP_SELF'] == 'login.php') {
-			header('Location: protected.php');
+	$user_session_complete_token = trim($_COOKIE['test_session']);
+	if (strpos($user_session_complete_token, ':') === false) {
+		// invalid token format
+		header('Location: /logout/');
+		die();
+	} else {
+		// using the proper session key/secret token system
+		$user_session_pieces = explode(':', $user_session_complete_token);
+		$user_session_key_db = "'".$mysqli->escape_string($user_session_pieces[0])."'";
+		$check_for_session = $mysqli->query("SELECT * FROM user_sessions WHERE session_key=$user_session_key_db AND expires > UNIX_TIMESTAMP()");
+		if ($check_for_session->num_rows == 1) {
+			// oh snap -- they might have a session if the secret matches!
+			$user_session_row = $check_for_session->fetch_assoc();
+			// validate secret
+			if (crypt($user_session_pieces[1], $user_session_row['session_secret']) != $user_session_row['session_secret']) {
+				// something is wrong, try again!
+				header('Location: logout.php');
+				die();
+			}
+			// ok, they're cool
+			$current_user_id = $user_session_row['user_id'];
+			$current_user['loggedin'] = true;
+			$current_user['user_id'] = $current_user_id;
+			$new_session_key_expires = time() + (60*60*24*30);
+			setcookie('test_session', $user_session_complete_token, $new_session_key_expires, '/', 'cylesoft.com');
+			$update_session_expiry = $mysqli->query("UPDATE user_sessions SET expires=$new_session_key_expires WHERE session_key=$user_session_key_db AND user_id=$current_user_id");
+			if ($_SERVER['PHP_SELF'] == 'login.php') {
+				header('Location: protected.php');
+				die();
+			}
+		} else {
+			// session is expired, make them log in again!
+			header('Location: logout.php');
 			die();
 		}
-	} else {
-		// session is expired, make them log in again!
-		header('Location: logout.php');
-		die();
 	}
 	
 } else if (isset($_POST['e']) && isset($_POST['p'])) { // user is trying to log in?
@@ -64,7 +78,7 @@ if (isset($_COOKIE['test_session']) && trim($_COOKIE['test_session']) != '') { /
 		$current_user_row = $check_for_user->fetch_assoc();
 		
 		// check password
-		if (crypt(trim($_POST['p']), $current_user_row['password']) != $current_user_row['password']) {
+		if (crypt(trim($_POST['p']), $current_user_row['pwrdlol']) != $current_user_row['pwrdlol']) {
 			die('Your password was incorrect, please try again.');
 		}
 		
@@ -72,13 +86,20 @@ if (isset($_COOKIE['test_session']) && trim($_COOKIE['test_session']) != '') { /
 		$current_user['loggedin'] = true;
 		$current_user['user_id'] = (int) $current_user_row['user_id'] * 1;
 		$current_user_id = $current_user['user_id'];
-		$new_session_key = get_key(256);
-		$new_session_key_expires = time() + (60*60*24*30);
-		setcookie('test_session', $new_session_key, $new_session_key_expires, '/', 'cylesoft.com');
 		
-		// write session to database
+		// set up new session token
+		$new_session_key = get_key(256);
+		$new_session_secret = get_key(256);
+		$new_session_complete_token = $new_session_key.':'.$new_session_secret;
+		$new_session_secret_salt = substr(get_key(256), 0, 22); // make a new 22-character salt
+		$new_session_secret_hash = crypt($new_session_secret, '$2y$12$' . $new_session_secret_salt);
+		$new_session_key_expires = time() + (60*60*24*30);
+		setcookie('test_session', $new_session_complete_token, $new_session_key_expires, '/', 'cylesoft.com');
+		
+		// write session to database with hashed secret
 		$new_session_key_db = "'".$mysqli->escape_string($new_session_key)."'";
-		$new_session_row = $mysqli->query("INSERT INTO user_sessions (session_id, user_id, expires) VALUES ($new_session_key_db, $current_user_id, $new_session_key_expires)");
+		$new_session_secret_hash_db = "'".$mysqli->escape_string($new_session_secret_hash)."'";
+		$new_session_row = $mysqli->query("INSERT INTO user_sessions (session_key, session_secret, user_id, expires, ts) VALUES ($new_session_key_db, $new_session_secret_hash_db, $current_user_id, $new_session_key_expires, UNIX_TIMESTAMP())");		
 		
 		// logged in, cool
 		header('Location: protected.php');
--- user_tables.sql
+++ user_tables.sql
@@ -1,14 +1,16 @@
 CREATE TABLE IF NOT EXISTS `user_sessions` (
-  `session_id` varchar(42) NOT NULL,
+  `session_key` varchar(255) NOT NULL,
+  `session_secret` varchar(255) NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
   `expires` int(11) NOT NULL,
-  PRIMARY KEY (`session_id`)
+  `ts` int(11) NOT NULL,
+  PRIMARY KEY (`session_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
+  `pwrdlol` varchar(250) NOT NULL,
   `tsc` int(11) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
